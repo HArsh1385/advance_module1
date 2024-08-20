a72b67e0e5f47caedc1ee7fac9a5e2db14362584
naman221462 <142684749+naman221462@users.noreply.github.com>
2024-08-21 01:26:27 +0530

    Update GameToken.sol

--- GameToken.sol
+++ GameToken.sol
@@ -1,43 +1,81 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-contract GameToken {
-    string public name="The NumWarrior";
-    string public symbol= "TNWR";
-    uint public totalSupply;
-    mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
 
-    event Transfer(address indexed from,address indexed to,uint value);
-    event Approval(address indexed owner,address indexed spender,uint value);
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+contract GameToken is IERC20 {
+    string public name = "The NumWarrior";
+    string public symbol = "TNWR";
+    uint8 public decimals = 18;
+    uint private _totalSupply;
+    mapping(address => uint) private _balances;
+    mapping(address => mapping(address => uint)) private _allowances;
+
+    address private _owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Not authorized");
+        _;
+    }
 
     constructor() {
-        mint(msg.sender, 1000); 
+        _owner = msg.sender;
+        _mint(msg.sender, 1000 * 10 ** uint(decimals)); // Mint initial supply
     }
 
-    function mint(address to,uint amount) public {
-        totalSupply += amount;
-        balanceOf[to] += amount;
-        emit Transfer(address(0), to, amount);
+    function totalSupply() public view override returns (uint) {
+        return _totalSupply;
     }
-    function transfer(address to,uint amount) public returns (bool) {
-        require(balanceOf[msg.sender] >= amount,"Oops!Insufficient balance in the account");
-        balanceOf[msg.sender]-= amount;
-        balanceOf[to]+= amount;
-        emit Transfer(msg.sender, to, amount);
+
+    function balanceOf(address account) public view override returns (uint) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint amount) public override returns (bool) {
+        require(_balances[msg.sender] >= amount, "Insufficient balance");
+        _balances[msg.sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
         return true;
     }
-    function approve(address spender, uint amount) public returns (bool) {
-        allowance[msg.sender][spender] = amount;
+
+    function allowance(address owner, address spender) public view override returns (uint) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint amount) public override returns (bool) {
+        _allowances[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
-    function transferFrom(address from, address to, uint amount) public returns (bool) {
-        require(balanceOf[from] >= amount, "Insufficient balance");
-        require(allowance[from][msg.sender] >= amount, "Allowance exceeded");
-        balanceOf[from] -= amount;
-        balanceOf[to] += amount;
-        allowance[from][msg.sender] -= amount;
-        emit Transfer(from, to, amount);
+
+    function transferFrom(address sender, address recipient, uint amount) public override returns (bool) {
+        require(_balances[sender] >= amount, "Insufficient balance");
+        require(_allowances[sender][msg.sender] >= amount, "Allowance exceeded");
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        _allowances[sender][msg.sender] -= amount;
+        emit Transfer(sender, recipient, amount);
         return true;
+    }
+
+    function mint(address account, uint amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function _mint(address account, uint amount) internal {
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
     }
 }
